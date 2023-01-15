18dd4421d3793be99a6933defd5072ff18141456
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-01-15 12:15:20 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import time
+from typing import Optional
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+# Trying to use default profile to avoid having to log in, but doesn't work...
+# options = webdriver.ChromeOptions()
+# options.add_argument(
+#     'user-data-dir=/Users/edward/Library/Application Support/Google/Chrome/Default'
+# )
+# driver = webdriver.Chrome(options=options)
+
+print('Press Ctrl + C at any moment to stop the bot.')
+
+driver = webdriver.Chrome()
+
+# Navigate to the Miyoo Mini website
+driver.get('https://s.click.aliexpress.com/e/_DDDi82J')
+# driver.get('https://s.click.aliexpress.com/e/_DBjkjv9') # test with ANBERNIC RG35XX
+
+
+def find_buy_button_element() -> Optional[WebElement]:
+    '''Tries tries to find the buy button'''
+    try:
+        return driver.find_element(By.CLASS_NAME, 'buy-now-wrap')
+    except NoSuchElementException:
+        return
+
+
+input('Log in in the Chrome window that has opened and press enter.')
+
+
+buy_button = find_buy_button_element()
+
+while buy_button != 'Buy Now':
+    print('No stock yet. Refreshing in 1/2 second.')
+    time.sleep(0.5)
+    driver.refresh()
+    buy_button = find_buy_button_element()
+
+input('It seems there is stock! BUY QUICKLY! (double enter to quick)')
+input()
+driver.quit()
