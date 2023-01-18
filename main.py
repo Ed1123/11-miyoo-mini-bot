1290840be9601d1a9c9102bea5a90a99123e3300
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-01-17 22:08:10 -0500

    Implement __buy method
    Create a __find_element method
    Correct names for internal (private) methods (pep8)

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Bot:
         options.add_argument('user-data-dir=profile')
         self.driver = webdriver.Chrome(options=options)
 
-    def __start_bot__(self):
+    def __start_bot(self):
         logging.info('Starting bot. Press Ctrl + C at any moment to close.')
         # Navigate to the Miyoo Mini website
         self.driver.get(MIYOO_MINI_WEBSITE)
@@ -41,8 +41,8 @@ class Bot:
             'Press enter to start refreshing...\n'
         )
 
-    def __check_stock__(self):
-        buy_button = self.__find_buy_button_element__()
+    def __check_stock(self):
+        buy_button = self.__find_buy_button_element()
 
         while buy_button.text != 'Buy Now':
             logging.info(
@@ -50,30 +50,48 @@ class Bot:
             )
             time.sleep(SECONDS_BETWEEN_REFRESH)
             self.driver.refresh()
-            buy_button = self.__find_buy_button_element__()
+            buy_button = self.__find_buy_button_element()
 
-    def __find_buy_button_element__(self) -> WebElement:
-        '''Tries tries to find the buy button'''
+    def __find_element(self, by: str, value: str) -> WebElement:
+        '''Tries to find element button'''
         try:
-            element = self.driver.find_element(By.CLASS_NAME, 'buy-now-wrap')
+            element = self.driver.find_element(by, value)
             element.text
         except (StaleElementReferenceException, NoSuchElementException):
             logging.info('Could not find buy button, refreshing.')
             self.driver.refresh()
-            element = self.__find_buy_button_element__()
+            element = self.__find_buy_button_element()
         return element
+
+    def __find_buy_button_element(self) -> WebElement:
+        '''Tries to find the buy button'''
+        return self.__find_element(By.CLASS_NAME, 'buy-now-wrap')
 
     def quit(self):
         logging.info('Closing bot.')
         self.driver.quit()
 
-    def __buy__(self):
-        raise NotImplementedError
+    def __buy(self):
+        # Click buy button
+        logging.info('Clicking buy button')
+        buy_button = self.__find_buy_button_element()
+        buy_button.click()
+
+        # Click to confirm the buy
+        logging.info('Clicking confirm button')
+        confirm_button = self.__find_element(
+            By.CLASS_NAME,
+            'comet-btn comet-btn-primary comet-btn-large pl-order-toal-container__btn',
+        )
+        confirm_button.click()
+
+        logging.info('Success 😃?')
+        input('Enter to quit...')
 
     def start_without_buying(self):
         try:
-            self.__start_bot__()
-            self.__check_stock__()
+            self.__start_bot()
+            self.__check_stock()
             input('It seems there is stock! BUY QUICKLY! (double enter to quick)')
             input()
         except KeyboardInterrupt:
@@ -81,16 +99,16 @@ class Bot:
 
     def start_with_buying(self):
         try:
-            self.__start_bot__()
-            self.__check_stock__()
-            self.__buy__()
+            self.__start_bot()
+            self.__check_stock()
+            self.__buy()
         except KeyboardInterrupt:
             self.quit()
 
 
 def main():
     bot = Bot()
-    bot.start_without_buying()
+    bot.start_with_buying()
 
 
 if __name__ == '__main__':
